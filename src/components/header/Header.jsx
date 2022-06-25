37829eef846cfca82547907ad657c00b4570cd67
Alec Dziwanowski <adziwanowski@gmail.com>
2022-06-25 15:54:08 -0400

    continued header styling changes. removed unused assets

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
         <div>
-          <img src={ME} alt="photo of Alec" />
+          <img src={ME} alt="photo of Alec" className="me"/>
         </div>
         <a href="#contact" className="scroll_down">Scroll Down</a>
       </div>
