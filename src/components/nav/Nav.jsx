088c35617ada62293d60920f0d2c5f260f7705c5
Alec Dziwanowski <adziwanowski@gmail.com>
2022-06-29 10:40:03 -0400

    removed unneeded sections. added ids so navbar links worked

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import './nav.css';
 import {AiOutlineHome} from 'react-icons/ai';
 import {BiUserCircle} from 'react-icons/bi';
 import {MdOutlineComputer} from 'react-icons/md';
+import {BiMessageDetail} from 'react-icons/bi';
 
 const Nav = () => {
   return (
@@ -10,8 +11,7 @@ const Nav = () => {
       <a href="#"><AiOutlineHome /></a>
       <a href="#about"><BiUserCircle /></a>
       <a href="#experience"><MdOutlineComputer /></a>
-      {/* <a href="#services"><FaHome /></a> */}
-      {/* <a href="#contact"><FaHome /></a> */}
+      <a href="#contact"><BiMessageDetail /></a>
     </nav>
   )
 }
