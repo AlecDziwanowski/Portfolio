212689727cf237d5eed7c01ced10b5f8ad441d2c
Alec Dziwanowski <adziwanowski@gmail.com>
2022-07-01 15:59:31 -0400

    minor css changes. changed into wording

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className='container header_container'>
-        <h5>Hi, I am</h5>
+        <h5>Hi, I'm</h5>
         <h1>Alec Dziwanowski</h1>
         <h5 className='text-light'>Full-Stack Developer</h5>
         <HeaderSocials />
