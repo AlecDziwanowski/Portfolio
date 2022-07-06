df4a4cca6447af5d8560b7745a892d94858c31d3
Alec Dziwanowski <adziwanowski@gmail.com>
2022-07-06 10:22:34 -0400

    continued edits to about.css. completed experience.jsx. began experience.css

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,37 +1,92 @@
 import React from 'react';
 import './experience.css';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
-import { FiPenTool } from 'react-icons/fi';
+// import { FiPenTool } from 'react-icons/fi';
 
 const Experience = () => {
   return (
     <section id='experience'>
-      <article className='about_card'>
-        <FiPenTool className='about_icon' />
-        <h5>Skills</h5>
-        <div className='flex'>
-          <div className='flex flex-col'>
-            <small><AiOutlineCheckCircle /> JavaScript</small>
-            <small><AiOutlineCheckCircle /> Node.js</small>
-            <small><AiOutlineCheckCircle /> React.js</small>
-            <small><AiOutlineCheckCircle /> Express.js</small>
-            <small><AiOutlineCheckCircle /> MongoDB</small>
-            <small><AiOutlineCheckCircle /> MERN Stack</small>
-            <small><AiOutlineCheckCircle /> MySQL2</small>
-            <small><AiOutlineCheckCircle /> GraphQL</small>
-          </div>
-          <div className='flex flex-col'>
-            <small><AiOutlineCheckCircle /> Handlebars</small>
-            <small><AiOutlineCheckCircle /> GIT / Version Control</small>
-            <small><AiOutlineCheckCircle /> Agile Development</small>
-            <small><AiOutlineCheckCircle /> HTML5 / CSS3</small>
-            <small><AiOutlineCheckCircle /> Bootstrap / Tailwind</small>
-            <small><AiOutlineCheckCircle /> C#</small>
-            <small><AiOutlineCheckCircle /> MatLab</small>
-            <small><AiOutlineCheckCircle /> Microsoft Office</small>
+      <h5>My</h5>
+      <h2>Skills</h2>
+      <div className='container experience_container'>
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>JavaScript</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>React.js</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>HTML5 / CSS3</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>Bootstrap / Tailwind</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>Handlebars</h4>
+            </article>
           </div>
         </div>
-      </article>
+
+        <div className="experience_backend">
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>Node.js</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>Express.js</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>MongoDB</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>MySQL2</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>GraphQL</h4>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience_frontend">
+          <h3>Auxillary Skills</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>GIT / Version Control</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>Agile Development</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>C#</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>MatLab</h4>
+            </article>
+            <article className="experience_details">
+              <AiOutlineCheckCircle />
+              <h4>Microsoft Office</h4>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
