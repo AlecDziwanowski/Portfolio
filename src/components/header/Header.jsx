651f36fc4b306341fb24232b47bb84b090d76dec
Alec Dziwanowski <adziwanowski@gmail.com>
2022-07-06 09:39:20 -0400

    modified about card positioning

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => {
   return (
     <header>
       <div className='container header_container'>
-        <h5>Hi, I'm</h5>
         <h1>Alec Dziwanowski</h1>
         <h5 className='text-light'>Full-Stack Developer</h5>
         <HeaderSocials />
