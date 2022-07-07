d7666084763d07efb1c294a47db4dc44d1fa0aa3
Alec Dziwanowski <adziwanowski@gmail.com>
2022-07-06 23:38:49 -0400

    experience to skills. changed vh scaling for portfolio sections

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,86 +1,85 @@
 import React from 'react';
-import './experience.css';
+import './skills.css';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
-// import { FiPenTool } from 'react-icons/fi';
 
-const Experience = () => {
+const Skills = () => {
   return (
-    <section id='experience'>
+    <section id='skills'>
       <h5>My</h5>
       <h2>Skills</h2>
-      <div className='container experience_container'>
-        <div className="experience_frontend">
+      <div className='container skills_container'>
+        <div className="skills_frontend">
           <h3>Frontend Development</h3>
-          <div className="experience_content">
-            <article className="experience_details">
+          <div className="skills_content">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>JavaScript</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>React.js</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>HTML5 / CSS3</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>Bootstrap / Tailwind</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>Handlebars</h4>
             </article>
           </div>
         </div>
 
-        <div className="experience_backend">
+        <div className="skills_backend">
           <h3>Backend Development</h3>
-          <div className="experience_content">
-            <article className="experience_details">
+          <div className="skills_content">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>Node.js</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>Express.js</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>MongoDB</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>MySQL2</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>GraphQL</h4>
             </article>
           </div>
         </div>
 
-        <div className="experience_auxillary">
+        <div className="skills_auxillary">
           <h3>Auxillary Skills</h3>
-          <div className="experience_content">
-            <article className="experience_details">
+          <div className="skills_content">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>GIT / Version Control</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>Agile Development</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>C#</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>MatLab</h4>
             </article>
-            <article className="experience_details">
+            <article className="skills_details">
               <AiOutlineCheckCircle />
               <h4>Microsoft Office</h4>
             </article>
@@ -91,4 +90,4 @@ const Experience = () => {
   )
 }
 
-export default Experience;
+export default Skills;
