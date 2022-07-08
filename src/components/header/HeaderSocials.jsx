273ae10978932d19786e3f1b1a1f5db8cb42be5c
Alec Dziwanowski <adziwanowski@gmail.com>
2022-07-08 12:23:39 -0400

    updated media queries for socials. updated socials scaling.

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
     return (
         <div className='header_socials'>
-            <a href='https://www.linkedin.com/in/alecdziwanowski/' target='_blank'><BsLinkedin /></a>
-            <a href='https://github.com/AlecDziwanowski' target='_blank'><BsGithub /></a>
+            <a href='https://www.linkedin.com/in/alecdziwanowski/' target='_blank' rel='noreferrer'><BsLinkedin size={28} /></a>
+            <a href='https://github.com/AlecDziwanowski' target='_blank' rel='noreferrer'><BsGithub size={28} /></a>
         </div>
     )
 }
