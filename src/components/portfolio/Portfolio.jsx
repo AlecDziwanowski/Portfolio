67e32203dd542653db5cb5f723fe90fef39dfa1e
Alec Dziwanowski <adziwanowski@gmail.com>
2022-07-08 00:00:30 -0400

    first pass at portfolio.jsx and images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/bookSearchEngine_SS.png';
+import IMG2 from '../../assets/SocialNetworkAPI_SS.png';
+import IMG3 from '../../assets/ECommerceBackend_SS.png';
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My</h5>
+      <h2>Recent Work</h2>
+
+      <div className='container portfolio_container'>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src={IMG1} alt='Book Search Engine' />
+          </div>
+          <h3>Book Search Engine</h3>
+          <a href='https://github.com/AlecDziwanowski/Book-Search-Engine' target='_blank' ClassName='btn'>Github Repo</a>
+          <a href='https://book-search-engine-20220606.herokuapp.com' target='_blank' ClassName='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src={IMG2} alt='Book Search Engine' />
+          </div>
+          <h3>Book Search Engine</h3>
+          <a href='https://github.com/AlecDziwanowski/Book-Search-Engine' target='_blank' ClassName='btn'>Github Repo</a>
+          <a href='https://book-search-engine-20220606.herokuapp.com' target='_blank' ClassName='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src={IMG3} alt='Book Search Engine' />
+          </div>
+          <h3>Book Search Engine</h3>
+          <a href='https://github.com/AlecDziwanowski/Book-Search-Engine' target='_blank' ClassName='btn'>Github Repo</a>
+          <a href='https://book-search-engine-20220606.herokuapp.com' target='_blank' ClassName='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+
     </section>
   )
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,78 +8,78 @@ const Skills = () => {
       <h5>My</h5>
       <h2>Skills</h2>
       <div className='container skills_container'>
-        <div className="skills_frontend">
+        <div className='skills_frontend'>
           <h3>Frontend Development</h3>
-          <div className="skills_content">
-            <article className="skills_details">
+          <div className='skills_content'>
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>JavaScript</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>React.js</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>HTML5 / CSS3</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>Bootstrap / Tailwind</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>Handlebars</h4>
             </article>
           </div>
         </div>
 
-        <div className="skills_backend">
+        <div className='skills_backend'>
           <h3>Backend Development</h3>
-          <div className="skills_content">
-            <article className="skills_details">
+          <div className='skills_content'>
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>Node.js</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>Express.js</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>MongoDB</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>MySQL2</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>GraphQL</h4>
             </article>
           </div>
         </div>
 
-        <div className="skills_auxillary">
+        <div className='skills_auxillary'>
           <h3>Auxillary Skills</h3>
-          <div className="skills_content">
-            <article className="skills_details">
+          <div className='skills_content'>
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>GIT / Version Control</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>Agile Development</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>C#</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>MatLab</h4>
             </article>
-            <article className="skills_details">
+            <article className='skills_details'>
               <AiOutlineCheckCircle />
               <h4>Microsoft Office</h4>
             </article>
